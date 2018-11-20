bf403e6a9c2449afeb6cc9442b262458ab808049
Samuel Breese <samuel@chame.co>
2018-11-19 20:32:57 -0500

    Lots of cleanup

--- src/Final/Cipher.hs
+++ src/Final/Cipher.hs
@@ -1,26 +1,54 @@
+{-# LANGUAGE UndecidableInstances #-}
+
 module Final.Cipher where
 
 import Data.Kind
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 
-data Encrypted (a :: Type) :: Type where
-  Wrapped :: BS.ByteString -> Encrypted a
+data Implementation_ a e d = Implementation_
+  { encrypt_ :: e -> ByteString -> ByteString
+  , decrypt_ :: d -> ByteString -> ByteString
+  , parseEncryptionKey_ :: ByteString -> e
+  , parseDecryptionKey_ :: ByteString -> d
+  }
 
 class Cipher (a :: Type) where
-  type family EncryptionKey (a :: Type) = (b :: Type) | b -> a
-  type family DecryptionKey (a :: Type) = (b :: Type) | b -> a
-  parseEncryptionKey :: ByteString -> EncryptionKey a
-  parseDecryptionKey :: ByteString -> DecryptionKey a
-  encrypt :: EncryptionKey a -> ByteString -> ByteString
-  decrypt :: DecryptionKey a -> ByteString -> ByteString
+  type family EncryptionKey a = (b :: Type) | b -> a
+  type family DecryptionKey a = (b :: Type) | b -> a
+  type family Implementation a :: Type
+  type Implementation a = Implementation_ a (EncryptionKey a) (DecryptionKey a)
+  implementation :: Implementation_ a (EncryptionKey a) (DecryptionKey a)
+
+encrypt :: Cipher a => EncryptionKey a -> ByteString -> ByteString
+encrypt = encrypt_ implementation
+decrypt :: Cipher a => DecryptionKey a -> ByteString -> ByteString
+decrypt = decrypt_ implementation
+parseEncryptionKey :: Cipher a => ByteString -> EncryptionKey a
+parseEncryptionKey = parseEncryptionKey_ implementation
+parseDecryptionKey :: Cipher a => ByteString -> DecryptionKey a
+parseDecryptionKey = parseDecryptionKey_ implementation
+
+data CipherMap :: Type where
+  None :: CipherMap
+  Some :: forall (a :: Type) (e :: Type) (d :: Type). Int -> Implementation_ a e d -> CipherMap -> CipherMap
+
+encryptWith :: CipherMap -> Int -> ByteString -> ByteString -> Maybe ByteString
+encryptWith None _ _ _ = Nothing
+encryptWith (Some i c rest) i' k d
+  | i == i' = pure $ encrypt_ c (parseEncryptionKey_ c k) d
+  | otherwise = encryptWith rest i' k d
 
 data IDSymmetric
 instance Cipher IDSymmetric where
   type EncryptionKey IDSymmetric = ()
   type DecryptionKey IDSymmetric = ()
-  encrypt _ = BS.reverse
-  decrypt _ = BS.reverse
+  implementation = Implementation_
+    { encrypt_ = const BS.reverse
+    , decrypt_ = const BS.reverse
+    , parseEncryptionKey_ = const ()
+    , parseDecryptionKey_ = const ()
+    }
 
 data IDPKC
 data E1 = E1
@@ -28,49 +56,17 @@ data E2 = E2
 instance Cipher IDPKC where
   type EncryptionKey IDPKC = E1
   type DecryptionKey IDPKC = E2
-  encrypt _ = id
-  decrypt _ = id
-  
-data CipherAbstract a = CipherAbstract { encrypt_ :: EncryptionKey a -> ByteString -> ByteString
-                                       , decrypt_ :: DecryptionKey a -> ByteString -> ByteString
-                                       , parseEncryptionKey_ :: ByteString -> EncryptionKey a
-                                       , parseDecryptionKey_ :: ByteString -> DecryptionKey a
-                                       }
-
-genericEncrypt :: Cipher a => CipherAbstract a -> ByteString -> ByteString -> ByteString
-genericEncrypt c = encrypt_ c . parseEncryptionKey_ c
-
-newtype CipherConcrete = CipherConcrete (forall (a :: Type). Cipher a => CipherAbstract a)
-
--- foo :: CipherConcrete -> ByteString -> ByteString -> ByteString
--- foo (CipherConcrete c) = genericEncrypt c
-
-data CipherMap :: Type where
-  None :: CipherMap
-  Some :: forall (a :: Type). Cipher a => Int -> CipherAbstract a -> CipherMap -> CipherMap
-
-lookupCipherEncrypt :: CipherMap -> Int -> ByteString -> ByteString -> Maybe ByteString
-lookupCipherEncrypt None _ _ _ = Nothing
-lookupCipherEncrypt (Some i c rest) i' k d | i == i' = pure $ encrypt_ c (parseEncryptionKey_ c k) d | otherwise = lookupCipherEncrypt rest i' k d
-
-idSymmetricAbstract :: CipherAbstract IDSymmetric
-idSymmetricAbstract = CipherAbstract { encrypt_ = encrypt
-                                     , decrypt_ = decrypt
-                                     , parseEncryptionKey_ = parseEncryptionKey
-                                     , parseDecryptionKey_ = parseDecryptionKey
-                                     }
-
-idPKCAbstract :: CipherAbstract IDPKC
-idPKCAbstract = CipherAbstract { encrypt_ = encrypt
-                               , decrypt_ = decrypt
-                               , parseEncryptionKey_ = parseEncryptionKey
-                               , parseDecryptionKey_ = parseDecryptionKey
-                               }
+  implementation = Implementation_
+    { encrypt_ = const id
+    , decrypt_ = const id
+    , parseEncryptionKey_ = const E1
+    , parseDecryptionKey_ = const E2
+    }
 
 foo :: CipherMap
-foo  = Some 1337 idSymmetricAbstract
-       . Some 31337 idPKCAbstract
-       $ None
+foo = Some 1337 (implementation :: Implementation IDSymmetric)
+      . Some 31337 (implementation :: Implementation IDPKC)
+      $ None
 
-bar :: Maybe ByteString
-bar = lookupCipherEncrypt foo 1337 "the key" "hello, world"
+bar :: Maybe ByteString 
+bar = encryptWith foo 1337 "the key" "hello, world"
