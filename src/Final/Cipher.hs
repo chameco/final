35629f4f1f603d200b74bc66d37c2487d5282e19
Samuel Breese <samuel@chame.co>
2018-11-19 21:18:48 -0500

    Hashes, more cleanup

--- src/Final/Cipher.hs
+++ src/Final/Cipher.hs
@@ -1,53 +1,69 @@
 {-# LANGUAGE UndecidableInstances #-}
 
-module Final.Cipher where
+module Final.Cipher
+  ( Implementation(..)
+  , Cipher, Impl, impl
+  , Lookup(..)
+  , encryptWith, decryptWith
+  ) where
 
 import Data.Kind
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
+import Data.Text (Text)
 
-data Implementation_ a e d = Implementation_
-  { encrypt_ :: e -> ByteString -> ByteString
-  , decrypt_ :: d -> ByteString -> ByteString
-  , parseEncryptionKey_ :: ByteString -> e
-  , parseDecryptionKey_ :: ByteString -> d
+data Implementation (a :: Type) (e :: Type) (d :: Type) (p :: Type) (c :: Type) = Implementation
+  { encrypt :: e -> p -> c
+  , decrypt :: d -> c -> p
+  , parseEncryptionKey :: ByteString -> e
+  , parseDecryptionKey :: ByteString -> d
+  , parsePlaintext :: ByteString -> p
+  , renderPlaintext :: p -> ByteString
+  , parseCiphertext :: ByteString -> c
+  , renderCiphertext :: c -> ByteString
   }
 
 class Cipher (a :: Type) where
-  type family EncryptionKey a = (b :: Type) | b -> a
-  type family DecryptionKey a = (b :: Type) | b -> a
-  type family Implementation a :: Type
-  type Implementation a = Implementation_ a (EncryptionKey a) (DecryptionKey a)
-  implementation :: Implementation_ a (EncryptionKey a) (DecryptionKey a)
+  type family EncryptionKey a :: Type
+  type family DecryptionKey a :: Type
+  type family Plaintext a :: Type
+  type family Ciphertext a :: Type
+  type family Impl a :: Type
+  type Impl a = Implementation a (EncryptionKey a) (DecryptionKey a) (Plaintext a) (Ciphertext a)
+  impl :: Implementation a (EncryptionKey a) (DecryptionKey a) (Plaintext a) (Ciphertext a)
 
-encrypt :: Cipher a => EncryptionKey a -> ByteString -> ByteString
-encrypt = encrypt_ implementation
-decrypt :: Cipher a => DecryptionKey a -> ByteString -> ByteString
-decrypt = decrypt_ implementation
-parseEncryptionKey :: Cipher a => ByteString -> EncryptionKey a
-parseEncryptionKey = parseEncryptionKey_ implementation
-parseDecryptionKey :: Cipher a => ByteString -> DecryptionKey a
-parseDecryptionKey = parseDecryptionKey_ implementation
+data Lookup :: Type -> Type where
+  None :: forall (k :: Type). Eq k => Lookup k
+  Some :: forall (k :: Type) (a :: Type) (e :: Type) (d :: Type) (p :: Type) (c :: Type).
+    Eq k => k -> Implementation a e d p c -> Lookup k -> Lookup k
 
-data CipherMap (k :: Type) :: Type where
-  NoCipher :: Eq k => CipherMap k
-  SomeCipher :: forall (a :: Type) (e :: Type) (d :: Type) (k :: Type). Eq k => k -> Implementation_ a e d -> CipherMap k -> CipherMap k
+encryptWith :: Eq k => Lookup k -> k -> ByteString -> ByteString -> Maybe ByteString
+encryptWith None _ _ _ = Nothing
+encryptWith (Some i c rest) i' key x
+  | i == i' = pure . renderCiphertext c . encrypt c (parseEncryptionKey c key) $ parsePlaintext c x
+  | otherwise = encryptWith rest i' key x
 
-encryptWith :: Eq k => CipherMap k -> k -> ByteString -> ByteString -> Maybe ByteString
-encryptWith NoCipher _ _ _ = Nothing
-encryptWith (SomeCipher i c rest) i' k d
-  | i == i' = pure $ encrypt_ c (parseEncryptionKey_ c k) d
-  | otherwise = encryptWith rest i' k d
+decryptWith :: Eq k => Lookup k -> k -> ByteString -> ByteString -> Maybe ByteString
+decryptWith None _ _ _ = Nothing
+decryptWith (Some i c rest) i' key x
+  | i == i' = pure . renderPlaintext c . decrypt c (parseDecryptionKey c key) $ parseCiphertext c x
+  | otherwise = encryptWith rest i' key x
 
 data IDSymmetric
 instance Cipher IDSymmetric where
   type EncryptionKey IDSymmetric = ()
   type DecryptionKey IDSymmetric = ()
-  implementation = Implementation_
-    { encrypt_ = const BS.reverse
-    , decrypt_ = const BS.reverse
-    , parseEncryptionKey_ = const ()
-    , parseDecryptionKey_ = const ()
+  type Plaintext IDSymmetric = ByteString
+  type Ciphertext IDSymmetric = ByteString
+  impl = Implementation
+    { encrypt = const BS.reverse
+    , decrypt = const BS.reverse
+    , parseEncryptionKey = const ()
+    , parseDecryptionKey = const ()
+    , parsePlaintext = id
+    , renderPlaintext = id
+    , parseCiphertext = id
+    , renderCiphertext = id
     }
 
 data IDPKC
@@ -56,17 +72,23 @@ data E2 = E2
 instance Cipher IDPKC where
   type EncryptionKey IDPKC = E1
   type DecryptionKey IDPKC = E2
-  implementation = Implementation_
-    { encrypt_ = const id
-    , decrypt_ = const id
-    , parseEncryptionKey_ = const E1
-    , parseDecryptionKey_ = const E2
+  type Plaintext IDPKC = ByteString
+  type Ciphertext IDPKC = ByteString
+  impl = Implementation
+    { encrypt = const id
+    , decrypt = const id
+    , parseEncryptionKey = const E1
+    , parseDecryptionKey = const E2
+    , parsePlaintext = id
+    , renderPlaintext = id
+    , parseCiphertext = id
+    , renderCiphertext = id
     }
 
-foo :: CipherMap Int
-foo = SomeCipher 1337 (implementation :: Implementation IDSymmetric)
-      . SomeCipher 31337 (implementation :: Implementation IDPKC)
-      $ NoCipher
+foo :: Lookup Text
+foo = Some "IDSymmetric" (impl :: Impl IDSymmetric)
+      . Some "IDPKC" (impl :: Impl IDPKC)
+      $ None
 
 bar :: Maybe ByteString 
-bar = encryptWith foo 1337 "the key" "hello, world"
+bar = encryptWith foo "IDSymmetric" "the key" "hello, world"
--- src/Final/Hash.hs
+++ src/Final/Hash.hs
@@ -1,1 +1,35 @@
-module Final.Hash where
+{-# LANGUAGE UndecidableInstances #-}
+
+module Final.Hash
+  ( Implementation(..)
+  , Hash, Impl, impl
+  , Lookup(..)
+  , hashWith
+  ) where
+
+import Data.Kind
+import Data.ByteString (ByteString)
+
+data Implementation (a :: Type) (p :: Type) (h :: Type) = Implementation
+  { hash :: p -> h
+  , parsePlaintext :: ByteString -> p
+  , renderHashtext :: h -> ByteString
+  }
+
+class Hash (a :: Type) where
+  type family Plaintext a :: Type
+  type family Hashtext a :: Type
+  type family Impl a :: Type
+  type Impl a = Implementation a (Plaintext a) (Hashtext a)
+  impl :: Implementation a (Plaintext a) (Hashtext a)
+
+data Lookup (k :: Type) :: Type where
+  NoHash :: forall (k :: Type). Eq k => Lookup k
+  SomeHash :: forall (k :: Type) (a :: Type) (p :: Type) (h :: Type).
+    Eq k => k -> Implementation a p h -> Lookup k -> Lookup k
+
+hashWith :: Eq k => Lookup k -> k -> ByteString -> Maybe ByteString
+hashWith NoHash _ _ = Nothing
+hashWith (SomeHash i c rest) i' d
+  | i == i' = pure . renderHashtext c . hash c $ parsePlaintext c d
+  | otherwise = hashWith rest i' d
