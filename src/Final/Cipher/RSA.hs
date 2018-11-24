f3c5f699e1d6d6ddfa3a603d07652ee9c2ff7080
Samuel Breese <samuel@chame.co>
2018-11-24 18:10:27 -0500

    Key serialization for RSA

--- src/Final/Cipher/RSA.hs
+++ src/Final/Cipher/RSA.hs
@@ -1,10 +1,32 @@
 module Final.Cipher.RSA where
 
-import Data.Binary (encode)
+import Control.Exception.Safe
+
+import Data.Word (Word8)
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as BS
 
 import Final.Cipher
 import Final.Utility.Modular
 import Final.Utility.Prime
+
+throwLeft :: (Exception e, MonadThrow m) => Either e a -> m a
+throwLeft (Left e) = throwM e 
+throwLeft (Right x) = pure x
+
+byteStringToInteger :: ByteString -> Integer
+byteStringToInteger = foldr (\x y -> y * 256 + fromIntegral x) 0 . unpad . BS.unpack
+  where unpad (0:bs) = unpad bs
+        unpad x = x
+
+integerToByteString :: Int -> Integer -> ByteString
+integerToByteString padding = BS.pack . pad . rep
+  where rep :: Integer -> [Word8]
+        rep 0 = []
+        rep n = fromIntegral (rem n 256):rep (quot n 256)
+        pad :: [Word8] -> [Word8]
+        pad l | length l >= padding = l
+              | otherwise = pad (0:l)
 
 data RSA
 instance Cipher RSA where
@@ -24,12 +46,22 @@ instance Cipher RSA where
            then generateDecryptionKey (impl :: Impl RSA) gen''
            else ((d, p, q), gen''')
     , deriveEncryptionKey = \(d, p, q) -> (modInv d $ carmichaelTotient p q, p * q)
-    , parseEncryptionKey = undefined
-    , renderEncryptionKey = encode
-    , parseDecryptionKey = undefined
-    , renderDecryptionKey = encode
-    , parsePlaintext = undefined
-    , renderPlaintext = encode
-    , parseCiphertext = undefined
-    , renderCiphertext = encode
+    , parseEncryptionKey = \bs -> let (ebs, nbs) = BS.splitAt 2048 bs
+                                  in if BS.length bs == 2 * 2048
+                                     then pure (byteStringToInteger ebs, byteStringToInteger nbs)
+                                     else throwString "Invalid RSA encryption key"
+    , renderEncryptionKey = \(e, n) -> integerToByteString 2048 e <> integerToByteString 2048 n
+    , parseDecryptionKey = \bs -> let (dbs, rest) = BS.splitAt 2048 bs
+                                      (pbs, qbs) = BS.splitAt 2048 rest
+                                  in if BS.length bs == 3 * 2048
+                                     then pure (byteStringToInteger dbs, byteStringToInteger pbs, byteStringToInteger qbs)
+                                     else throwString "Invalid RSA decryption key"
+    , renderDecryptionKey = \(d, p, q) -> mconcat [ integerToByteString 2048 d
+                                                  , integerToByteString 2048 p
+                                                  , integerToByteString 2048 q
+                                                  ]
+    , parsePlaintext = pure . byteStringToInteger
+    , renderPlaintext = integerToByteString 0
+    , parseCiphertext = pure . byteStringToInteger
+    , renderCiphertext = integerToByteString 0
     }
