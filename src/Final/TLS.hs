79b42bdbea175d8c5b5c44a97b12dff4e0ca83ee
Samuel Breese <samuel@chame.co>
2018-12-02 18:59:20 -0500

    ClientHello up to Server Name extension

--- src/Final/TLS.hs
+++ src/Final/TLS.hs
@@ -3,12 +3,54 @@ module Final.TLS where
 import Data.Word (Word8)
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy as BS
+import qualified Data.ByteString as BS.S
 
 import Network.Socket hiding (recv)
 import Network.Socket.ByteString (recv, sendAll)
 
-clientSendHello :: Socket -> IO ()
-clientSendHello = undefined
+import Final.Utility.ByteString
+
+addHandshakeRecordHeader :: ByteString -> BS.S.ByteString
+addHandshakeRecordHeader d = BS.S.pack $ mconcat
+  [ [ 0x16 -- Handshake record
+    , 0x03, 0x03 -- TLS 1.2
+    ]
+  , BS.unpack . integerToByteStringBE 2 . fromIntegral $ BS.length d
+  , BS.unpack d
+  ]
+
+buildExtensionServerName :: ByteString -> ByteString
+buildExtensionServerName hostname = BS.pack $ mconcat
+  [ [ 0x00, 0x00 -- Server name extension
+    ]
+  , BS.unpack . integerToByteStringBE 2 $ (len + 5) -- Length of extension data
+  , BS.unpack . integerToByteStringBE 2 $ (len + 3) -- Length of list entry
+  , [ 0x00 -- List entry is a DNS hostname
+    ]
+  , BS.unpack . integerToByteStringBE 2 $ len -- Length of hostname
+  , BS.unpack hostname
+  ]
+  where len = fromIntegral $ BS.length hostname
+
+clientSendHello :: Socket -> ByteString -> ByteString -> IO ()
+clientSendHello sock rand hostname = sendAll sock . addHandshakeRecordHeader . BS.pack $ mconcat
+  [ [ 0x01 -- Client hello
+    -- 3-byte length here
+    , 0x03, 0x03 -- TLS 1.2
+    ]
+  , BS.unpack rand
+  , [ 0x00 -- Don't provide a session ID
+    , 0x00, 0x02 -- Two bytes of ciphersuite data follow
+    , 0xcc, 0xa8 -- We support TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256
+    , 0x01 -- One byte of compression method data follows
+    , 0x00 -- No compression
+    ]
+  , BS.unpack . integerToByteStringBE 2 . fromIntegral $ BS.length extensions -- Length of extensions
+  , BS.unpack extensions
+  ]
+  where extensions = mconcat
+          [ buildExtensionServerName hostname
+          ]
 
 clientRecvHello :: Socket -> IO ByteString -- Assume server supports everything we request
 clientRecvHello = undefined
