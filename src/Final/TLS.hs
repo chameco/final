1a684f4a6db83ec9506d91e9fe5837e5a61e3be5
Samuel Breese <samuel@chame.co>
2018-12-02 19:38:29 -0500

    Refactor interface

--- src/Final/TLS.hs
+++ src/Final/TLS.hs
@@ -3,15 +3,11 @@ module Final.TLS where
 import Data.Word (Word8)
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy as BS
-import qualified Data.ByteString as BS.S
-
-import Network.Socket hiding (recv)
-import Network.Socket.ByteString (recv, sendAll)
 
 import Final.Utility.ByteString
 
-addHandshakeRecordHeader :: ByteString -> BS.S.ByteString
-addHandshakeRecordHeader d = BS.S.pack $ mconcat
+addHandshakeRecordHeader :: ByteString -> ByteString
+addHandshakeRecordHeader d = BS.pack $ mconcat
   [ [ 0x16 -- Handshake record
     , 0x03, 0x03 -- TLS 1.2
     ]
@@ -26,58 +22,8 @@ addHandshakeHeader t d = BS.pack $ mconcat
   , BS.unpack . integerToByteStringBE 3 . fromIntegral $ BS.length d
   ]
 
-buildExtensionServerName :: ByteString -> ByteString
-buildExtensionServerName hostname = BS.pack $ mconcat
-  [ [ 0x00, 0x00 -- Server name extension
-    ]
-  , BS.unpack . integerToByteStringBE 2 $ (len + 5) -- Length of extension data
-  , BS.unpack . integerToByteStringBE 2 $ (len + 3) -- Length of list entry
-  , [ 0x00 -- List entry is a DNS hostname
-    ]
-  , BS.unpack . integerToByteStringBE 2 $ len -- Length of hostname
-  , BS.unpack hostname
-  ]
-  where len = fromIntegral $ BS.length hostname
-
-buildExtensionSupportedGroups :: ByteString
-buildExtensionSupportedGroups = BS.pack
-  [ 0x00, 0x0a -- Supported groups extension
-  , 0x00, 0x04 -- 4 bytes of supported groups data follow
-  , 0x00, 0x02 -- 2 bytes of data in the supported groups list
-  , 0x00, 0x1d -- Assigned value for x25519
-  ]
-
-buildExtensionECPointsFormat :: ByteString
-buildExtensionECPointsFormat = BS.pack
-  [ 0x00, 0x0b -- EC points format extension
-  , 0x00, 0x02 -- 2 bytes of EC points format data follow
-  , 0x01 -- 1 byte of data in the supported groups list
-  , 0x00 -- No compression
-  ]
-
-buildExtensionSignatureAlgorithms :: ByteString
-buildExtensionSignatureAlgorithms = BS.pack
-  [ 0x00, 0x0d -- Signature algorithms extension
-  , 0x00, 0x04 -- 4 bytes of signature algorithms data follow
-  , 0x00, 0x02 -- 2 bytes of data in the algorithms list
-  , 0x04, 0x01 -- Assigned value for RSA/PKCS1/SHA256
-  ]
-
-buildExtensionRenegotiationInfo :: ByteString
-buildExtensionRenegotiationInfo = BS.pack
-  [ 0xff, 0x01 -- Renegotiation info extension
-  , 0x00, 0x01 -- 1 byte of renegotiation info data follows
-  , 0x00 -- This is a new connection
-  ]
-
-buildExtensionSCT :: ByteString
-buildExtensionSCT = BS.pack
-  [ 0x00, 0x12 -- SCT extension
-  , 0x00, 0x00 -- 0 bytes of SCT data follow
-  ]
-
-clientSendHello :: Socket -> ByteString -> ByteString -> IO ()
-clientSendHello sock rand hostname = sendAll sock . addHandshakeRecordHeader . addHandshakeHeader 0x01 . BS.pack $ mconcat
+clientBuildHello :: ByteString -> ByteString -> ByteString
+clientBuildHello rand hostname = addHandshakeRecordHeader . addHandshakeHeader 0x01 . BS.pack $ mconcat
   [ [ 0x03, 0x03 -- TLS 1.2
     ]
   , BS.unpack rand
@@ -91,48 +37,86 @@ clientSendHello sock rand hostname = sendAll sock . addHandshakeRecordHeader . a
   , BS.unpack extensions
   ]
   where extensions = mconcat
-          [ buildExtensionServerName hostname
-          , buildExtensionSupportedGroups
-          , buildExtensionECPointsFormat
-          , buildExtensionSignatureAlgorithms
-          , buildExtensionRenegotiationInfo
-          , buildExtensionSCT
+          [ extensionServerName
+          , extensionSupportedGroups
+          , extensionECPointsFormat
+          , extensionSignatureAlgorithms
+          , extensionRenegotiationInfo
+          , extensionSCT
+          ]
+        extensionServerName = BS.pack $ mconcat
+          [ [ 0x00, 0x00 -- Server name extension
+            ]
+          , BS.unpack . integerToByteStringBE 2 $ (len + 5) -- Length of extension data
+          , BS.unpack . integerToByteStringBE 2 $ (len + 3) -- Length of list entry
+          , [ 0x00 -- List entry is a DNS hostname
+            ]
+          , BS.unpack . integerToByteStringBE 2 $ len -- Length of hostname
+          , BS.unpack hostname
+          ]
+          where len = fromIntegral $ BS.length hostname
+        extensionSupportedGroups = BS.pack
+          [ 0x00, 0x0a -- Supported groups extension
+          , 0x00, 0x04 -- 4 bytes of supported groups data follow
+          , 0x00, 0x02 -- 2 bytes of data in the supported groups list
+          , 0x00, 0x1d -- Assigned value for x25519
+          ]
+        extensionECPointsFormat = BS.pack
+          [ 0x00, 0x0b -- EC points format extension
+          , 0x00, 0x02 -- 2 bytes of EC points format data follow
+          , 0x01 -- 1 byte of data in the supported groups list
+          , 0x00 -- No compression
+          ]
+        extensionSignatureAlgorithms = BS.pack
+          [ 0x00, 0x0d -- Signature algorithms extension
+          , 0x00, 0x04 -- 4 bytes of signature algorithms data follow
+          , 0x00, 0x02 -- 2 bytes of data in the algorithms list
+          , 0x04, 0x01 -- Assigned value for RSA/PKCS1/SHA256
+          ]
+        extensionRenegotiationInfo = BS.pack
+          [ 0xff, 0x01 -- Renegotiation info extension
+          , 0x00, 0x01 -- 1 byte of renegotiation info data follows
+          , 0x00 -- This is a new connection
+          ]
+        extensionSCT = BS.pack
+          [ 0x00, 0x12 -- SCT extension
+          , 0x00, 0x00 -- 0 bytes of SCT data follow
           ]
 
-clientRecvHello :: Socket -> IO ByteString -- Assume server supports everything we request
-clientRecvHello = undefined
+clientParseHello :: ByteString -> () -- Assume server supports everything we request
+clientParseHello = undefined
 
-clientRecvCert :: Socket -> IO ()
-clientRecvCert = undefined
+clientParseCert :: ByteString -> ()
+clientParseCert = undefined
 
-clientRecvKey :: Socket -> IO ByteString
-clientRecvKey = undefined
+clientParseKey :: ByteString -> ByteString
+clientParseKey = undefined
 
-clientRecvHelloDone :: Socket -> IO ()
-clientRecvHelloDone = undefined
+clientParseHelloDone :: ByteString -> ()
+clientParseHelloDone = undefined
 
-clientSendKeyExchange :: Socket -> ByteString -> IO ()
-clientSendKeyExchange sock pub = sendAll sock . addHandshakeRecordHeader . addHandshakeHeader 0x10 $ mconcat
+clientBuildKeyExchange :: ByteString -> ByteString
+clientBuildKeyExchange pub = addHandshakeRecordHeader . addHandshakeHeader 0x10 $ mconcat
   [ integerToByteStringBE 1 . fromIntegral $ BS.length pub
   , pub
   ]
 
-clientSendChangeCipherSpec :: Socket -> IO ()
-clientSendChangeCipherSpec sock = sendAll sock $ BS.S.pack
+clientBuildChangeCipherSpec :: ByteString
+clientBuildChangeCipherSpec = BS.pack
   [ 0x14 -- Change cipher spec record
   , 0x03, 0x03 -- TLS 1.2
   , 0x00, 0x01 -- 1 byte of change cipher spec data follows
   , 0x01
   ]
 
-clientSendHandshakeFinished :: Socket -> ByteString -> ByteString -> IO ()
-clientSendHandshakeFinished sock eiv ctext = sendAll sock . addHandshakeRecordHeader $ mconcat
+clientBuildHandshakeFinished :: ByteString -> ByteString -> ByteString
+clientBuildHandshakeFinished eiv ctext = addHandshakeRecordHeader $ mconcat
   [ eiv
   , ctext
   ]
 
-clientRecvChangeCipherSpec :: Socket -> IO ()
-clientRecvChangeCipherSpec = undefined
+clientParseChangeCipherSpec :: ByteString -> ()
+clientParseChangeCipherSpec = undefined
 
-clientRecvHandshakeFinished :: Socket -> IO ()
-clientRecvHandshakeFinished = undefined
+clientParseHandshakeFinished :: ByteString -> ()
+clientParseHandshakeFinished = undefined
